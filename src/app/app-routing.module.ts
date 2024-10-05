2dc403d854a35845509196d81a5cf004248d27a4
JuliAugusta <juli_aug@hotmail.com>
2024-10-04 22:39:58 -0300

    inclusao das rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RulesComponent } from './rules/rules.component';
+import { PetListComponent } from './pet-list/pet-list.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'rules', component: RulesComponent}
+  { path: 'rules', component: RulesComponent},
+  { path: 'pets', component: PetListComponent}
 ];
 
 @NgModule({
