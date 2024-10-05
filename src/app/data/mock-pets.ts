3c191eec80ebdb6f2fc5cc5fa19921375a8fa0a6
JuliAugusta <juli_aug@hotmail.com>
2024-10-04 22:40:34 -0300

    criacao do mock dos pets

--- src/app/data/mock-pets.ts
+++ src/app/data/mock-pets.ts
@@ -1,5 +1,11 @@
 export const PETS_DATA = [
-  { id: 1, name: 'Rex', type: 'Dog', img: 'dog.png', favorited: false },
-  { id: 2, name: 'Mimi', type: 'Cat', img: 'cat.png', favorited: false },
-  { id: 3, name: 'Goldie', type: 'Fish', img: 'fish.png', favorited: false },
+  { id: 1, name: 'Luigy Gonzales',  type: 'Buldogue', gender: 'macho',  img: 'assets/img-animals/LuigyGonzales2x.png', favorited: false },
+  { id: 2, name: 'Bartolomeu II',  type: 'Vira-lata', gender: 'macho',  img: 'assets/img-animals/BartolomeuII2x.png', favorited: false },
+  { id: 3, name: 'Marieta Soares', type: 'Golden', gender: 'fêmea',  img: 'assets/img-animals/MarietaSoares2x.png', favorited: false },
+  { id: 4, name: 'Pity Caramelo', type: 'Vira-lata', gender: 'fêmea',  img: 'assets/img-animals/PityCaramelo2x.png', favorited: false },
+  { id: 5, name: 'Shakira Lopez', type: 'Amarelo', gender: 'fêmea',  img: 'assets/img-animals/ShakiraLopez2x.png', favorited: false },
+  { id: 6, name: 'Nico Di Angelo', type: 'Frajola', gender: 'macho',  img: 'assets/img-animals/NicoDiAngelo2x.png', favorited: false },
+  { id: 7, name: 'Perseu Baptist', type: 'Rajado', gender: 'macho',  img: 'assets/img-animals/PerseuBaptist2x.png', favorited: false },
+  { id: 8, name: 'Teodora Blanc', type: 'Albino', gender: 'fêmea',  img: 'assets/img-animals/TheodoraBlanc2x.png', favorited: false },
+  { id: 9, name: 'Rosinha Mali', type: 'Escaminha', gender: 'fêmea',  img: 'assets/img-animals/RosinhaMali2x.png', favorited: false },
 ];
