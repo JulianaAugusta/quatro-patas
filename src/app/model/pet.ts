5f92a8ac31857ede594f7fdd3a55a829a6f52ff7
JuliAugusta <juli_aug@hotmail.com>
2024-10-07 17:25:31 -0300

    ajustes de layout

---
+++ src/app/model/pet.ts
@@ -0,0 +1,8 @@
+export interface Pet {
+  id: number;
+  name: string;
+  type: string;
+  gender: string;
+  img: string;
+  isFavorite: boolean;
+}
--- src/app/pet-list/pet-list.component.ts
+++ src/app/pet-list/pet-list.component.ts
@@ -1,38 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PetService } from '../services/pet.service';
+import { Pet } from '../model/pet';
 
 @Component({
   selector: 'app-pet-list',
   templateUrl: './pet-list.component.html',
   styleUrls: ['./pet-list.component.sass']
 })
-export class PetListComponent {
+export class PetListComponent implements OnInit {
 
-  pets: any[] = [];
-  showModalAdopt: boolean = false;
-  showModalFavorite: boolean = false;
+  pets: Pet[] = [];
+  showModal: boolean = false;
+  modalType: 'adopt' | 'favorite' | null = null; // Define o tipo da modal
 
   constructor(private petService: PetService) { }
 
   ngOnInit(): void {
-    this.petService.getPets().subscribe((data) => {
-      this.pets = data;
+    this.petService.getPets().subscribe((data: Pet[]) => {
+      this.pets = data.map(pet => ({ ...pet, isFavorite: false }));
     });
   }
 
   adoptPet(): void {
-    this.showModalAdopt = true;
+    this.showModal = true;
+    this.modalType = 'adopt';
   }
 
-  closeModalAdopt(): void {
-    this.showModalAdopt = false;
+  closeModal(): void {
+    this.showModal = false;
+    this.modalType = null;
   }
 
-  favoriteImage(): void {
-    this.showModalFavorite = true;
-  }
+  favoriteImage(pet: Pet): void {
+    pet.isFavorite = !pet.isFavorite;
 
-  closeModalFavorite(): void {
-    this.showModalFavorite = false;
+    if (pet.isFavorite) {
+      this.showModal = true;
+      this.modalType = 'favorite';
+    }
   }
 }
